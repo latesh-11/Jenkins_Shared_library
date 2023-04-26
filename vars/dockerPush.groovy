7059cda5d71d343d4c55eb2ab687465046540120
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 01:00:57 +0530

    docker image scan

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -4,9 +4,6 @@ def call( String project , String imageTag , String userName ){
             docker login -u ${userName} -p ${passwd}
             docker image push ${userName}/${project}:v1.${imageTag}
             docker image push ${userName}/${project}:latest
-
-
-            sh 'docker image rm -f ${project}:v1.${imageTag} ${userName}/${project}:v1.${imageTag} docker image push ${userName}/${project}:latest'
         '''
     }
 }
