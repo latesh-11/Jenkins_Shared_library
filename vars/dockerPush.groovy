a60a704665f66694d5ec5bfb7fcbaaf1059b4773
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 00:21:51 +0530

    docker image push

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,8 +2,8 @@ def call( String project , String imageTag , String userName ){
     withCredentials([string(credentialsId: 'docker-key', variable: 'passwd')]) {
         sh'''
             docker login -u ${userName} -p ${passwd}
-            docker imaeg push ${userName}/${project}:v1.${imageTag}
-            docker imaeg push ${userName}/${project}:latest
+            docker image push ${userName}/${project}:v1.${imageTag}
+            docker image push ${userName}/${project}:latest
 
 
             sh 'docker image rm -f ${JOB_NAME}:v1.${BUILD_ID} lateshh/${JOB_NAME}:v1.${BUILD_ID} lateshh/${JOB_NAME}:latest '
