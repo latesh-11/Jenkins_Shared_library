98103b874ac10eb6d1c697df395c5f56a7c96824
latesh-11 <sharmalatesh125@gmail.com>
2023-04-26 23:24:47 +0530

    docker build add

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
-def call(String project , String imageTag , Stirng userName) {
+def call(String project , String imageTag , String userName) {
       sh '''
         docker image build -t ${project}:v1.${imageTag} .
         docker image tag ${project}:v1.${imageTag} ${userName}/${project}:v1.${imageTag}
