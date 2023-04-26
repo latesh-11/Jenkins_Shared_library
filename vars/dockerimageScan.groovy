87baaaafba1713a58393e6a9fcf0c4cd4067a40e
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 01:05:56 +0530

    docker image scan

--- vars/dockerimageScan.groovy
+++ vars/dockerimageScan.groovy
@@ -1,6 +1,6 @@
 def call(String project , String imageTag , String userName ) {
     sh '''
-        trivy image ${project}:v1.${imageTag} ${userName}/${project}:latest > scan.txt
+        trivy image ${userName}/${project}:latest > scan.txt
         echo "Showing scan file"
         cat scan.txt
     '''
