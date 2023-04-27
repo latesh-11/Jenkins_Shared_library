58a3e661f6d600d8e1e15d16f2e7f29178928855
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 11:39:45 +0530

    docker scan

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -16,5 +16,7 @@ def call( String region , String accountID , String userName ) {
     sh """
         aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountID}.dkr.ecr.${region}.amazonaws.com
         docker push ${accountID}.dkr.ecr.${region}.amazonaws.com/${userName}:latest
+        
+        docker rm image -f ${accountID}.dkr.ecr.${region}.amazonaws.com/${userName}:latest
     """
 }
