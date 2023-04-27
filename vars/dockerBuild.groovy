15d1927ead9f63deb8a50f7cfa90025da1a2ae1c
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 12:45:38 +0530

    docker scan

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -7,11 +7,11 @@
 // }
 
 def call(String region , String accountID , String userName) {
-  withCredentials([<object of type com.cloudbees.jenkins.plugins.awscredentials.AmazonWebServicesCredentialsBinding>]) {
+  
     sh """
       docker build -t ${userName} .
       docker tag ${userName}:latest ${accountID}.dkr.ecr.${region}.amazonaws.com/${userName}:latest
 
     """
-  }
+  
 }
