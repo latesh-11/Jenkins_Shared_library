2c3afbe7995aad1483964cc2418ce4c3f881a68a
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 11:16:34 +0530

    docker push on ecr

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,15 @@
-def call(String project , String imageTag , String userName) {
-      sh '''
-        docker image build -t ${project}:v1.${imageTag} .
-        docker image tag ${project}:v1.${imageTag} ${userName}/${project}:v1.${imageTag}
-        docker image tag ${project}:v1.${imageTag} ${userName}/${project}:latest
-        '''
+// def call(String project , String imageTag , String userName) {
+//       sh '''
+//         docker image build -t ${project}:v1.${imageTag} .
+//         docker image tag ${project}:${imageTag} ${userName}/${project}:v1.${imageTag}
+//         docker image tag ${project}:${imageTag} ${userName}/${project}:latest
+//         '''
+// }
+
+def call(String region , String accountID , String userName) {
+  sh """
+    docker build -t ${userName} .
+    docker tag ${userName}:latest ${accountID}.dkr.ecr.${region}.amazonaws.com/${userName}:latest
+    
+  """
 }
--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,10 +2,10 @@ def call( String project , String imageTag , String userName ){
     withCredentials([string(credentialsId: 'docker-key', variable: 'passwd')]) {
         sh'''
             docker login -u ${userName} -p ${passwd}
-            docker image push ${userName}/${project}:v1.${imageTag}
+            docker image push ${userName}/${project}:${imageTag}
             docker image push ${userName}/${project}:latest
 
-            docker image rm -f ${userName}/${project}:v1.${imageTag}
+            docker image rm -f ${userName}/${project}:${imageTag}
             docker image rm -f ${userName}/${project}:latest
 
         '''
