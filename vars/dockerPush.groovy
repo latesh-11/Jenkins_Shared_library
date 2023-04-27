c9eedc37266bd8f567032b9e2240eb34321baf3a
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 11:28:41 +0530

    docker push on ecr

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,13 +1,17 @@
-def call( String project , String imageTag , String userName ){
-    withCredentials([string(credentialsId: 'docker-key', variable: 'passwd')]) {
-        sh'''
-            docker login -u ${userName} -p ${passwd}
-            docker image push ${userName}/${project}:${imageTag}
-            docker image push ${userName}/${project}:latest
+// def call( String project , String imageTag , String userName ){
+//     withCredentials([string(credentialsId: 'docker-key', variable: 'passwd')]) {
+//         sh'''
+//             docker login -u ${userName} -p ${passwd}
+//             docker image push ${userName}/${project}:${imageTag}
+//             docker image push ${userName}/${project}:latest
 
-            docker image rm -f ${userName}/${project}:${imageTag}
-            docker image rm -f ${userName}/${project}:latest
+//             docker image rm -f ${userName}/${project}:${imageTag}
+//             docker image rm -f ${userName}/${project}:latest
 
-        '''
-    }
+//         '''
+//     }
+// }
+
+def call( String region , String accountID , String userName ) {
+    sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountID}.dkr.ecr.${region}.amazonaws.com"
 }
