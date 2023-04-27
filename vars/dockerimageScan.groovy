c3644ce841ea46bff79fdf049d57a4fc6c3935d5
latesh-11 <sharmalatesh125@gmail.com>
2023-04-27 11:32:47 +0530

    docker scan

--- vars/dockerimageScan.groovy
+++ vars/dockerimageScan.groovy
@@ -1,7 +1,9 @@
-def call(String project , String imageTag , String userName ) {
-    sh '''
-        trivy image ${userName}/${project}:latest > scan.txt
+def call(String region , String accountID , String userName) {
+    sh """
+        trivy image ${userName}:latest ${accountID}.dkr.ecr.${region}.amazonaws.com/${userName}:latest > scan.txt
         echo "Showing scan file"
         cat scan.txt
-    '''
+    """
 }
+
+// trivy image ${userName}/${project}:latest > scan.txt
